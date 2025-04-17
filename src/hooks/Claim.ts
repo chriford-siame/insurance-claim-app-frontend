88361787cb4c1e06a58c9a607a7ff6db3ea84de3
chriford-siame <chrifordsiame@gmail.com>
2025-04-18 01:42:29 +0200

    Create a hook

---
+++ src/hooks/Claim.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { IClaim } from "src/interfaces/claim";
+
+const useClaim = (id: string | number) => {
+    const [claim, setClaim] = useState<IClaim | {}>({});
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+  
+    useEffect(() => {
+      const fetchClaim = async () => {
+        try {
+          const response = await axios.get<IClaim>(`http://localhost:8000/claims/${id}/`);
+          setClaim(response.data);
+        } catch (err) {
+          setError("Failed to fetch claim");
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      fetchClaim();
+    }, [id]);
+  
+    return { claim, loading, error };
+  };
+  
+export default useClaim;
